505a665fe089a1e759e5c16b1381700fa4f3a03a
Jean-Christophe Hoelt <hoelt@fovea.cc>
2016-04-23 04:11:54 -0700

    Style fixes

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, user-scalable=no, target-densitydpi=device-dpi">
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 <?php wp_head(); ?>
 </head>
